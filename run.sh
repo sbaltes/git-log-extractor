c1ab99c1c505789f925fe93b6738d2fd5692a054
Sebastian Baltes <s.baltes@uni-trier.de>
2018-02-06 07:09:25 +0100

    Fix path

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./clone_projects.sh input/comparison_project_sample_100 ABSOLUTE_OUTPUT_PATH > output.log 2>&1
+# always use absolute path for output directory
+./clone_projects.sh input/comparison_project_sample_100.csv /data/comparison_project_sample_100 > output.log 2>&1
