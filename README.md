21dc8f4ff5516f1e10e7ebf53f876da3c247628f
Sebastian Baltes <s.baltes@uni-trier.de>
2018-06-14 14:11:59 +0200

    Update README, add new input file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Clone a list of GitHub projects and save the git-log of all branches.
 
+Usage: Edit parameters in `run.sh` and execute that script.
+
 [![DOI](https://zenodo.org/badge/94237028.svg)](https://zenodo.org/badge/latestdoi/94237028)
 
 Based on code written by [Jascha Knack](https://github.com/jaschaknack/fst_ws1617_exercises/tree/master/exercise_04).
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # always use absolute path for output directory
-./clone_projects.sh input/comparison_project_sample_100.csv /data/logs > output_clone.log 2>&1
+./clone_projects.sh input/comparison_project_sample_200.csv /data/logs > output_clone.log 2>&1
 ./extract_and_merge_branch_csvs.sh /data/logs /data/branches > output_extract-branch-info.log 2>&1
