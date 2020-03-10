888e92fcfee9d7bfc458822c1e5117517d5a2cdf
Sebastian Baltes <research@sbaltes.com>
2020-03-11 09:45:11 +1030

    Refactor run script

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# configure this:
 base_dir="/Users/sebastian/git/tools/git-log-extractor" # remove trailing slash
-input_file="input/c.csv"
+input_file="c++.csv"
+
+# keep this:
+input_dir="$base_dir/input"
+input_file="$input_dir/$input_file"
 output_dir="$base_dir/output"
 mkdir -p "$output_dir"
 java_dir="$base_dir/java"
-java_class="GitLogFilter"
-log_file="output.log"
+filter_class="GitLogFilter"
+log_file="$base_dir/output.log"
+
+echo "Input file: $input_file"
+echo "Output dir: $output_dir"
+echo "Java dir: $java_dir"
+echo "Filter class: $filter_class"
+echo "Log file: $log_file"
 
 # always use absolute path for output directory
-./retrieve_commits.sh "$input_file" "$output_dir" "$java_dir" "$java_class" > "$log_file" 2>&1
+echo "Retrieving commits..."
+./retrieve_commits.sh "$input_file" "$output_dir" "$java_dir" "$filter_class" > "$log_file" 2>&1
